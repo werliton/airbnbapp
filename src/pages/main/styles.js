4dc39fa4c3df86480de95a6655fc280035157a30
werliton <werlitoncarlos@gmail.com>
2018-12-26 22:00:57 -0300

    Finalizando a tela signIn

---
+++ src/pages/main/styles.js
@@ -0,0 +1,21 @@
+const Container = styled.View`
+    flex:1;
+    alignItems: center;
+    justifyContent: center;
+    backgroundColor: #F5F5F5;
+`;
+const Logo = styled.Image`
+  height: 30%;
+  marginBottom: 40px;
+`;
+
+const Input = styled.TextInput`
+  paddingHorizontal: 20px;
+  paddingVertical: 15px;
+  borderRadius: 5px;
+  backgroundColor: #FFF;
+  alignSelf: stretch;
+  marginBottom: 15px;
+  marginHorizontal: 20px;
+  fontSize: 16px;
+`;
